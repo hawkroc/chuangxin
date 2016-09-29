f60582f3c7e47d94a03d337d0891b96c8a0b58dc
roc <yinpengroc@gmail.com>
2016-09-30 12:15:24 +1300

    add shedule

---
+++ src/main/java/com/fh/service/system/auto/AutoServiceTest.java
@@ -0,0 +1,14 @@
+package com.fh.service.system.auto;
+
+import org.springframework.stereotype.Service;
+
+@Service("autoService")
+public class AutoServiceTest {
+	
+	public void testTask() {
+		
+		System.out.println("this task be invoked");
+		
+	}
+
+}
